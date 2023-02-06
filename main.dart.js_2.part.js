13c4f83f71b92e5c23f05a5523c56d7649d227f1
Genildo Nogueira <64094937+GenildoNogueira@users.noreply.github.com>
2023-02-05 21:22:33 -0300

    Delete main.dart.js_2.part.js

--- main.dart.js_2.part.js
+++
@@ -1,7 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B,C
-A=c[13]
-B=c[0]
-C=c[2]
-var z=a.updateTypes([]);(function constants(){A.IU=new B.N(!0,null,null,null,null,null,18,C.cF,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null)})()}
-$__dart_deferred_initializers__["NINrM5GyDhE1cmkxXVQfnX7JmFc="] = $__dart_deferred_initializers__.current
